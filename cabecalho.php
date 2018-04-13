beedc1c64006bed495cbb311b4c69b5d257e8297
biancapastos <biancapastos12@gmail.com>
2018-04-13 09:41:35 -0300

    atualizacao Bianca

---
+++ cabecalho.php
@@ -0,0 +1,17 @@
+<?php
+
+	echo "<a href = 'index.php'>Index</a>";
+	
+	echo "&nbsp;&nbsp;|&nbsp;&nbsp;";
+	
+	echo "<a href = 'form_estado.php'>Cadastrar Estado</a>";
+	
+	echo "&nbsp;&nbsp;|&nbsp;&nbsp;";
+	
+	echo "<a href = 'form_cidade.php'>Cadastrar Cidade</a>";
+	
+	echo "&nbsp;&nbsp;|&nbsp;&nbsp;";
+	
+	echo "<a href = 'form_cadastro.php'>Cadastrar Usuario</a>";
+
+?>
